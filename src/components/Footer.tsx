5a731d9c22ba6f0909d7e04e92b5f787cf2b3494
Ho Man Boa <boa.homan@gmail.com>
2021-09-11 19:37:49 -0400

    Routes again

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ import {
 import AccessibilityNewIcon from '@material-ui/icons/AccessibilityNew';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import ListAltIcon from '@material-ui/icons/ListAlt';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useHistory, useLocation } from 'react-router-dom';
 
 import { mainTheme } from '../theme';
 
@@ -26,16 +26,17 @@ const useStyles = makeStyles((theme) => ({
 export default function Routes(): JSX.Element {
   const classes = useStyles(mainTheme);
   const location = useLocation();
+  const history = useHistory();
 
   return (
     <div className={classes.footerContainer}>
       {
         location.pathname == '/' &&
-        <Link to="/explore" className={classes.link}><ListAltIcon /></Link>
+        <Link to="#" className={classes.link} onClick={() => history.push("/explore")}><ListAltIcon /></Link>
       }
       {
         location.pathname == '/explore' &&
-        <Link to="/" className={classes.link}><AccessibilityNewIcon /></Link>
+        <Link to="#" className={classes.link} onClick={() => history.push("/")}><AccessibilityNewIcon /></Link>
       }
        <a href="https://github.com/aranair/spells-viewer" target="_blank" className={classes.link}>
          <GitHubIcon />
